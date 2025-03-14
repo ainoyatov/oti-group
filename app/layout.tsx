5887bbfd88b921cc6bb1df1c24d408b15a24c387
Aki Inoyatov <info@gravityelements.com>
2025-03-14 15:12:21 -0700

    top menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TopMenu from "@/components/navigation/TopMenu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          <TopMenu />
+        </div>
         {children}
       </body>
     </html>
---
+++ components/navigation/TopMenu.tsx
@@ -0,0 +1,32 @@
+import { NavLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function TopMenu() {
+
+    return (
+        <div className='flex min-h-auto flex-row justify-between px-4 sm:px-12 sm:mt-8'>
+            <a className='flex h-[100px] ' href='/'>
+               <Image src="/top-menu-logo.png" alt="main logo" width={150} height={1} className=''/> 
+            </a>
+
+            <div className="flex flex-row">
+                <div className="flex h-[100px] max-sm:hidden">
+                    <ul className="flex h-[100px] justify-center">
+                        {NavLinks.map((links) => (
+                            <li key={links.label} className="mt-4 p-2 text-xl  text-left">
+                                <Link
+                                    href={links.href}
+                                    className="text-2xl p-2"
+                                >
+                                    {links.label}
+                                </Link>
+                            </li>
+                        ))}  
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
