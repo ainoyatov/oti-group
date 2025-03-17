e2caf3bb46857fc3d4925d52d344e786227adafb
Aki Inoyatov <info@gravityelements.com>
2025-03-16 23:57:56 -0700

    call button added

--- components/navigation/TopMenu.tsx
+++ components/navigation/TopMenu.tsx
@@ -5,19 +5,19 @@ import Image from "next/image";
 export default function TopMenu() {
 
     return (
-        <div className='flex min-h-auto flex-row justify-between px-4 sm:px-12 sm:mt-8'>
+        <div className='flex min-h-auto flex-row justify-between  sm:px-6 sm:mt-8'>
             <a className='flex h-[100px] ' href='/'>
                <Image src="/top-menu-logo.png" alt="main logo" width={150} height={1} className=''/> 
             </a>
 
             <div className="flex flex-row">
                 <div className="flex h-[100px] max-lg:hidden">
-                    <ul className="flex h-[100px] justify-center">
+                    <ul className="flex h-[100px] items-center">
                         {NavLinks.map((links) => (
-                            <li key={links.label} className="mt-4 p-2 text-xl  text-left">
+                            <li key={links.label} className="p-2 text-left">
                                 <Link
                                     href={links.href}
-                                    className="text-2xl p-2"
+                                    className="text-2xl"
                                 >
                                     {links.label}
                                 </Link>
@@ -27,11 +27,9 @@ export default function TopMenu() {
                 </div>
             </div>
 
-            {/* <div className="flex">
-                <a href="tel:+16822677741" className="flex items-center p-4 text-white font-semibold bg-blue-500">
-                    682-267-7741
-                </a>
-            </div> */}
+            <div className="flex items-center text-2xl p-4 bg-blue-500 text-white max-lg:hidden">
+                682-267-7741
+            </div>
             
         </div>
     )
