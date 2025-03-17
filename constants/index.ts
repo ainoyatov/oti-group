5b7639499f6cdd1e7be389c7d86afe8a4c792c2c
Aki Inoyatov <info@gravityelements.com>
2025-03-16 21:14:49 -0700

    updated menu

--- constants/index.ts
+++ constants/index.ts
@@ -1,8 +1,7 @@
 export const NavLinks = [
-    {href: "/", label:"Home"},
     {href: "/about-us", label:"About Us"},
     {href: "/how-it-works", label:"How It Works"},
-    {href: "/faq", label:"FAQ"},
+    {href: "/blog", label:"Blog"},
     {href: "/contact-us", label:"Contact Us"},
     {href: "/appointment", label:"Appointment"},
 ]
