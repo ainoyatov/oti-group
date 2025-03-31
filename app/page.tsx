7d49c7c15010eaefa207e3fbbd919713c930930d
Aki Inoyatov <info@gravityelements.com>
2025-03-31 10:56:27 -0700

    home seo updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,16 @@ import FirstDiv from "@/components/main-page/FirstDiv";
 import FourthDiv from "@/components/main-page/FourthDiv";
 import SecondDiv from "@/components/main-page/SecondDiv";
 import ThirdDiv from "@/components/main-page/ThirdDiv";
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'We Buy Houses Cash - OT Investment Group',
+  description: '',
+  robots: {
+    follow: true,
+    index: true
+  },
+}
 
 export default function Home() {
 
